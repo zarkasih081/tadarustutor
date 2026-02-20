80aa98126fecb75c745ffc4599a8ca5cd1dc40e6
zarkasih081 <zarkasih081@gmail.com>
2026-02-20 14:43:42 +0700

    Update router.js

--- assets/js/modules/router.js
+++ assets/js/modules/router.js
@@ -1,1 +1,26 @@
+import { renderHome, renderTutorList, renderEbook, renderVideo, renderRegister } from './components.js';
 
+const routes = {
+    '': renderHome,
+    '#home': renderHome,
+    '#tutors': renderTutorList,
+    '#ebook': renderEbook,
+    '#video': renderVideo,
+    '#register': renderRegister
+};
+
+export function initRouter() {
+    const handleRoute = () => {
+        const hash = window.location.hash;
+        const renderer = routes[hash] || renderHome;
+        
+        const main = document.getElementById('main-content');
+        main.innerHTML = '';
+        main.appendChild(renderer());
+        
+        window.scrollTo(0,0);
+    };
+
+    window.addEventListener('hashchange', handleRoute);
+    handleRoute();
+}
