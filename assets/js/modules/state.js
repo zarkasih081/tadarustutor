e05d62927c56b46f9292441116e87296b34edbc4
zarkasih081 <zarkasih081@gmail.com>
2026-02-20 14:43:22 +0700

    Update state.js

--- assets/js/modules/state.js
+++ assets/js/modules/state.js
@@ -1,1 +1,12 @@
-
+export const state = {
+    tutors: [
+        { id: 1, name: "Ust. Hanif Al-Hafiz", skill: "Sanad", method: "Talaqqi", price: 150000, img: "https://ui-avatars.com/api/?name=Hanif" },
+        { id: 2, name: "Ustazah Fatimah", skill: "Tahsin", method: "Online", price: 100000, img: "https://ui-avatars.com/api/?name=Fatimah" }
+    ],
+    ebooks: [
+        { title: "Tajwid Dasar", author: "Tim TT", file: "tajwid.pdf" }
+    ],
+    videos: [
+        { title: "Makhraj Huruf", url: "#" }
+    ]
+};
